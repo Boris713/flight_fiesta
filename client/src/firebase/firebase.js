e81cd67d5e43185269e942cb6d5fa29b7ab38cd8
Boris Alexander Hernandez <borishernandez@meta.com>
2024-06-26 14:37:07 -0700

    Hide Firebase API info

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,18 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBQsJH27-KXBih2fRTgNWqQC4ZZCPapzoY",
-  authDomain: "flightfiesta-78afe.firebaseapp.com",
-  projectId: "flightfiesta-78afe",
-  storageBucket: "flightfiesta-78afe.appspot.com",
-  messagingSenderId: "468962218389",
-  appId: "1:468962218389:web:3d3075761037009af7e69c",
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env
+    .VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
