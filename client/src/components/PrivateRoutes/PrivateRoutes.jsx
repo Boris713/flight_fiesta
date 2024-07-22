848884adf02abfb05949901f111afbd0935092f3
Boris Alexander Hernandez <borishernandez@meta.com>
2024-07-22 12:04:06 -0700

    delete logs

--- client/src/components/PrivateRoutes/PrivateRoutes.jsx
+++ client/src/components/PrivateRoutes/PrivateRoutes.jsx
@@ -5,11 +5,7 @@ import { useAuth } from "../../contexts/authContexts/authContexts";
 const PrivateRoutes = () => {
   const { userLoggedIn, loading } = useAuth();
 
-  console.log("Loading state:", loading); // Logging loading state
-  console.log("User logged in state:", userLoggedIn); // Logging auth state
-
   if (loading) {
-    // Optionally, you can return a loading spinner here
     return <div>Loading...</div>;
   }
 
