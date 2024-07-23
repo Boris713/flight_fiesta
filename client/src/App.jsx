6084d30c34111c7c750bb57b7ba7706097589523
Boris Hernandez Jr. <108431983+Boris713@users.noreply.github.com>
2024-07-23 16:13:15 -0700

    Protected routes (#10)
    * Logic for city based recommendations
    
    * self code review
    
    * Add protected routes
    
    * delete logs
    
    * logs 2
    
    ---------
    
    Co-authored-by: Boris Alexander Hernandez <borishernandez@meta.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Authentication from "./pages/Authentication/Authentication";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import ItineraryCreator from "./pages/ItineraryCreator/ItineraryCreator";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CityProvider } from "./contexts/cityContext/cityContext";
+import PrivateRoutes from "./components/PrivateRoutes/PrivateRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <CityProvider>
         <Header />
         <Routes>
-          <Route path="/" element={<Authentication />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/itinerary-creator" element={<ItineraryCreator />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/itinerary-creator" element={<ItineraryCreator />} />
+          </Route>
+          <Route path="/" element={<Authentication />} />
         </Routes>
       </CityProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
 
